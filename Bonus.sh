de3602fc6ccd670ecf8103c67df80de3cafff0f4
Qinyin Ling <lingqinyin@login1.ufhpc>
2018-09-17 09:19:45 -0400

    Bonus question finished

---
+++ Bonus.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo -n "Please enter either airport-code or city-name or state-name (case-sensitive) -> "
+read name
+len="$(echo "$name" | wc -c)"
+if [[ "$name" =~ [[0-9] ]];then
+  echo "Invalid Input"
+else
+if ((len>2));then
+    grep -w "$name" flights.May2017-Apr2018.csv | cut -d "," -f 13,16 | grep -c 1.00
+else
+  echo "Invalid Input"
+  fi
+fi
